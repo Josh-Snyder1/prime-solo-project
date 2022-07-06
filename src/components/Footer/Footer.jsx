b6759947d227d2a04303ec1ac03b985757141b0b
Josh Snyder <joshrsnyder@gmail.com>
2022-07-06 12:56:18 -0500

    footer and header set

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,38 @@
-import React from 'react';
 import './Footer.css';
 
-// This is one of our simplest components
-// It doesn't have local state, so it can be a function component.
-// It doesn't dispatch any redux actions or display any part of redux state
-// or even care what the redux state is, so it doesn't need 'connect()'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import RestoreIcon from '@mui/icons-material/Restore';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import Paper from '@mui/material/Paper'
+import LibraryMusicIcon from '@mui/icons-material/LibraryMusic';
+import SearchIcon from '@mui/icons-material/Search';
 
-function Footer() {
-  return <footer>&copy; Prime Digital Academy</footer>;
+
+export default function SimpleBottomNavigation() {
+  const [value, setValue] = React.useState(0);
+
+  return (
+    <>
+    <Box sx={{ width: 500 }}>
+        <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
+          <BottomNavigation
+            showLabels
+            value={value}
+            onChange={(event, newValue) => {
+              setValue(newValue);
+            }}
+          >
+            <BottomNavigationAction label="Collection" icon={<LibraryMusicIcon />}/>
+            <BottomNavigationAction label="Wishlist" icon={<FavoriteIcon />}/>
+            <BottomNavigationAction label="Browse" icon={<SearchIcon />} />
+          </BottomNavigation>
+        </Paper>
+      </Box>
+    </>
+  );
 }
 
-export default Footer;
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,47 +1,116 @@
-import React from 'react';
+import * as React from 'react';
+import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import Paper from '@mui/material/Paper'
 import { Link } from 'react-router-dom';
-import LogOutButton from '../LogOutButton/LogOutButton';
-import './Nav.css';
-import { useSelector } from 'react-redux';
 
-function Nav() {
-  const user = useSelector((store) => store.user);
+// import './Nav.css';
+
+const options = [
+  'My Profile',
+  'Info',
+  'About',
+  'Logout',
+];
+
+const ITEM_HEIGHT = 48;
+
+export default function Nav() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
-    <div className="nav">
-      <Link to="/homePage">
-        <h2 className="nav-title">Prime Solo Project</h2>
-      </Link>
+    <Paper sx={{ position: 'fixed', top: 0, left: 0, right: 0 }} elevation={3}>
+    <div>
+      
+    <Link to="/homePage">
+      <h2 className="nav-title">Canoe MN</h2>
+    </Link>
+
       <div>
-        {/* If no user is logged in, show these links */}
-        {!user.id && (
-          // If there's no user, show login/registration links
-          <Link className="navLink" to="/landingPage">
-            Login / Register
-          </Link>
-        )}
-
-        {/* If a user is logged in, show these links */}
-        {user.id && (
-          <>
-            <Link className="navLink" to="/homePage">
-              Home
-            </Link>
-
-            <Link className="navLink" to="/info">
-              Info Page
-            </Link>
-
-            <LogOutButton className="navLink" />
-          </>
-        )}
-
-        <Link className="navLink" to="/about">
-          About
-        </Link>
+      <IconButton
+        sx={{ position: 'fixed', top: 10, right: 0 }} elevation={3}
+        aria-label="more"
+        id="long-button"
+        aria-controls={open ? 'long-menu' : undefined}
+        aria-expanded={open ? 'true' : undefined}
+        aria-haspopup="true"
+        onClick={handleClick}
+      >
+        <MoreVertIcon />
+      </IconButton>
+      <Menu
+        id="long-menu"
+        MenuListProps={{
+          'aria-labelledby': 'long-button',
+        }}
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        PaperProps={{
+          style: {
+            maxHeight: ITEM_HEIGHT * 4.5,
+            width: '20ch',
+          },
+        }}
+      >
+        {options.map((option) => (
+          <MenuItem key={option} selected={option === 'Pyxis'} onClick={handleClose}>
+            {option}
+          </MenuItem>
+        ))}
+      </Menu>
       </div>
     </div>
+    </Paper>
   );
 }
 
-export default Nav;
+
+
+
+
+
+
+
+// <div className="nav">
+// <Link to="/homePage">
+//   <h2 className="nav-title">Prime Solo Project</h2>
+// </Link>
+// <div>
+//   {/* If no user is logged in, show these links */}
+//   {!user.id && (
+//     // If there's no user, show login/registration links
+//     <Link className="navLink" to="/landingPage">
+//       Login / Register
+//     </Link>
+//   )}
+
+//   {/* If a user is logged in, show these links */}
+//   {user.id && (
+//     <>
+//       <Link className="navLink" to="/homePage">
+//         Home
+//       </Link>
+
+//       <Link className="navLink" to="/info">
+//         Info Page
+//       </Link>
+
+//       <LogOutButton className="navLink" />
+//     </>
+//   )}
+
+//   <Link className="navLink" to="/about">
+//     About
+//   </Link>
+// </div>
+// </div>
